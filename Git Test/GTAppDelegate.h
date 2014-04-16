e94639466c5d6066270ba03d1a4059128c7a7030
marco martinetti <marcomartinetti@iMac-di-marco.local>
2014-04-16 11:48:15 +0200

    Initial Commit

---
+++ Git Test/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  Git Test
+//
+//  Created by marco martinetti on 16/04/14.
+//  Copyright (c) 2014 marco martinetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/GTViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTViewController.h
+//  Git Test
+//
+//  Created by marco martinetti on 16/04/14.
+//  Copyright (c) 2014 marco martinetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTViewController : UIViewController
+
+@end
